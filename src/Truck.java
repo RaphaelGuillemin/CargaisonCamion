fd9e10ee70b0b62ae713ee991530f3e2a55cff9b
laurabegin <laurabegin1@gmail.com>
2019-06-12 16:39:46 -0400

    update comments in Truck

--- src/Truck.java
+++ src/Truck.java
@@ -1,43 +1,44 @@
 public class Truck {
-  private int capacity;
-  private int nBoxesToTransport;
-  private Coordinates coords;
-  private int nBoxes;
+    private int capacity;
+    private int nBoxesToTransport;
+    private Coordinates coords;
+    private int nBoxes;
 
-  public Truck(int nBoxesToTransport, int capacity) {
-    this.capacity = capacity;
-    this.nBoxesToTransport = nBoxesToTransport;
-  }
-
-  // Adds boxes to the truck and removes them from the building specified
-  void addBoxes(Building building){
-    int num = building.getNBoxes();
-    if (num > this.capacity){
-      num = this.capacity;
+    // Truck constructor
+    public Truck(int nBoxesToTransport, int capacity) {
+        this.capacity = capacity;
+        this.nBoxesToTransport = nBoxesToTransport;
     }
-    if (num > this.nBoxesToTransport - this.nBoxes) {
-      num = this.nBoxesToTransport - this.nBoxes; // Number of boxes left to transport
+
+    // Adds boxes to the truck and removes them from the building specified
+    void addBoxes(Building building) {
+        int num = building.getNBoxes();
+        if (num > this.capacity) {
+            num = this.capacity;
+        }
+        if (num > this.nBoxesToTransport - this.nBoxes) {
+            num = this.nBoxesToTransport - this.nBoxes; // Number of boxes left to transport
+        }
+        this.nBoxes += num;
+        this.capacity -= num;
+        // Remove boxes from the building
+        building.removeBoxes(num);
     }
-    this.nBoxes += num;
-    this.capacity -= num;
-    // Remove boxes from the building
-    building.removeBoxes(num);
-  }
 
-  // Sets the truck's position to the building with the most boxes
-  void setCoords(Building building){
-    this.coords = new Coordinates(building.getCoords().getLatitude(), building.getCoords().getLongitude());
-  }
+    // Sets the truck's position to the building with the most boxes
+    void setCoords(Building building) {
+        this.coords = new Coordinates(building.getCoords().getLatitude(), building.getCoords().getLongitude());
+    }
 
-  public Coordinates getCoords() {
-    return this.coords;
-  }
+    public Coordinates getCoords() {
+        return this.coords;
+    }
 
-  public int getNBoxes() {
-    return this.nBoxes;
-  }
+    public int getNBoxes() {
+        return this.nBoxes;
+    }
 
-  public int getNBoxesToTransport() {
-    return this.nBoxesToTransport;
-  }
+    public int getNBoxesToTransport() {
+        return this.nBoxesToTransport;
+    }
 }
