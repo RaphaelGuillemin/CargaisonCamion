74f31a9d2baa23b933036f7e2a54c8cbba4888be
raphaduqc <raphael.guillemin59@hotmail.fr>
2019-06-12 16:52:26 -0400

    testbench

--- src/Testbench.java
+++ src/Testbench.java
@@ -1,17 +1,19 @@
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 
 public class Testbench {
 
   public static void main(String[] args) throws Exception {
-      String[] arguments = new String[2];
-      for (int i=100;i<2505;i=i+50){
-        arguments[0] = "CargaisonCamion/src/tp1Input2/"+i+".txt";
-        arguments[1] = "CargaisonCamion/output/hey"+i+".txt";
+    String[] arguments = new String[2];
+    for (int i = 100; i <= 2500; i = i + 50) {
+      for (int j = 0; j <= 100; j++) {
+        arguments[0] = "CargaisonCamion/src/tp1Input2/" + i + ".txt";
+        arguments[1] = "CargaisonCamion/output/hey" + i + ".txt";
         double tempsIni = System.nanoTime();
         Tp1.main(arguments);
-        System.out.println("0,"+((""+((System.nanoTime() - tempsIni)/10E9)).substring(2)));
+        double tempsFinal = (System.nanoTime() - tempsIni) / 10E9;
+        String[] num = ("" + tempsFinal).split("[.]");
+        System.out.println(num[0] + "," + num[1]);
       }
-
+    }
 
   }
 
